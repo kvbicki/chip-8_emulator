b720eef95bf2f7eaf694306dea512133cf299619
skubicki342 <szymon.kubicki342@gmail.com>
2025-07-03 17:37:30 +0200

    LoadROM implemented

---
+++ chip8.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <cstdint>
+#include <random>
+
+const unsigned int KEY_COUNT = 16;
+const unsigned int MEMORY_SIZE = 4096;
+const unsigned int REGISTER_COUNT = 16;
+const unsigned int STACK_LEVELS = 16;
+const unsigned int VIDEO_HEIGHT = 32;
+const unsigned int VIDEO_WIDTH = 64;
+
+class Chip8 {
+public:
+    Chip8();
+    void LoadROM(char const* filename);
+    uint8_t registers[16]{};
+	uint8_t memory[4096]{};
+	uint16_t index{};
+	uint16_t pc{};
+	uint16_t stack[16]{};
+	uint8_t sp{};
+	uint8_t delayTimer{};
+	uint8_t soundTimer{};
+	uint8_t keypad[16]{};
+	uint32_t video[64 * 32]{};
+	uint16_t opcode;
+};
