16c79b31d1b4ab2efe88f591dd7f34c338850ec9
kvbicki <szymon.kubicki342@gmail.com>
2025-07-05 16:43:38 +0200

    Fx but not ended

--- chip8.cpp
+++ chip8.cpp
@@ -6,6 +6,7 @@
 #include <random>
 const unsigned int FONTSET_SIZE = 80;
 const unsigned int STARTING_ROM = 0x200;
+const unsigned int STARTING_FONSET = 0x50;
 
 uint8_t fontset[FONTSET_SIZE] =
 {
@@ -49,6 +50,11 @@ Chip8::Chip8()
 	: randGen(std::chrono::system_clock::now().time_since_epoch().count())
 {
 	pc = STARTING_ROM;
+	for (unsigned int i = 0; i < FONTSET_SIZE; ++i)
+	{
+		memory[STARTING_FONSET + i] = fontset[i];
+	}
+
 	index = 0;
 	sp = 0;
 	delayTimer = 0;
@@ -223,7 +229,106 @@ void Chip8::OP_Ex9E(){
 	if(keypad[registers[Vx]]) pc += 2;
 }
 
-void Chip8::OP_Ex9E(){
+void Chip8::OP_ExA1(){
 	u_int8_t Vx = (opcode & 0x0F00u) >> 8u;
 	if(!keypad[registers[Vx]]) pc += 2;
+}
+void Chip8::OP_Fx07(){
+	u_int8_t Vx = (opcode & 0x0F00u) >> 8u;
+	registers[Vx] = delayTimer;
+
+}
+void Chip8::OP_Fx0A(){
+	uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+	if (keypad[0])
+	{
+		registers[Vx] = 0;
+	}
+	else if (keypad[1])
+	{
+		registers[Vx] = 1;
+	}
+	else if (keypad[2])
+	{
+		registers[Vx] = 2;
+	}
+	else if (keypad[3])
+	{
+		registers[Vx] = 3;
+	}
+	else if (keypad[4])
+	{
+		registers[Vx] = 4;
+	}
+	else if (keypad[5])
+	{
+		registers[Vx] = 5;
+	}
+	else if (keypad[6])
+	{
+		registers[Vx] = 6;
+	}
+	else if (keypad[7])
+	{
+		registers[Vx] = 7;
+	}
+	else if (keypad[8])
+	{
+		registers[Vx] = 8;
+	}
+	else if (keypad[9])
+	{
+		registers[Vx] = 9;
+	}
+	else if (keypad[10])
+	{
+		registers[Vx] = 10;
+	}
+	else if (keypad[11])
+	{
+		registers[Vx] = 11;
+	}
+	else if (keypad[12])
+	{
+		registers[Vx] = 12;
+	}
+	else if (keypad[13])
+	{
+		registers[Vx] = 13;
+	}
+	else if (keypad[14])
+	{
+		registers[Vx] = 14;
+	}
+	else if (keypad[15])
+	{
+		registers[Vx] = 15;
+	}
+	else
+	{
+		pc -= 2;
+	}
+}
+void Chip8::OP_Fx15(){
+	uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+	delayTimer = registers[Vx];
+}
+void Chip8::OP_Fx18(){
+	uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+	soundTimer = registers[Vx];
+}
+void Chip8::OP_Fx1E(){
+	uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+	index += registers[Vx];
+}
+void Chip8::OP_Fx29()
+{
+	uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+	uint8_t digit = registers[Vx];
+
+	index = STARTING_FONSET + (5 * digit);
+}
+void Chip8::OP_Fx33(){
+	
 }
--- chip8.h
+++ chip8.h
@@ -53,4 +53,11 @@ public:
 	void OP_Dxyn();
 	void OP_Ex9E();
 	void OP_ExA1();
+	void OP_Fx07();
+	void OP_Fx0A();
+	void OP_Fx15();
+	void OP_Fx18();
+	void OP_Fx1E();
+	void OP_Fx29();
+	void OP_Fx33();
 };
